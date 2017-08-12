335707bac2b619907724a7b6fdf0375220fbfcbd
Ethan Rivers' Home Desktop <ethan@riversfamily.us>
2017-08-12 00:50:28 -0500

    old approach consumed too much memory and was far too inefficient, moving on to a matrix representation from a list representation

--- aho_corasick.h
+++ aho_corasick.h
@@ -7,32 +7,6 @@
 
 namespace aho_corasick
 {
-	class node
-	{
-	public:
-		node();
-		node(char);
-		~node();
-
-		char color() const;
-		void color(const char&);
-
-		char name() const;
-		void name(const char&);
-
-		friend node* createGraph(const std::vector<std::string>&);
-		friend void printGraph(node*);
-		friend void deleteGraph(node*);
-	private:
-		char _color, _name;
-		std::vector<node*> black;
-		std::vector<node*> blue;
-		std::vector<node*> green;
-	};
-
-	node* createGraph(const std::vector<std::string>&);
-	void printGraph(node*);
-	void deleteGraph(node*);
 };
 
 #endif //end __AHO_CORASICK_H__
