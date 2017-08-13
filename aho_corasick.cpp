2c5e961599658412f86e5e1be6112a1ec1ea2afd
Ethan Rivers' Home Desktop <ethan@riversfamily.us>
2017-08-12 19:03:52 -0500

    going to do a more advanced implementation using smart pointers etc.

--- aho_corasick.cpp
+++ aho_corasick.cpp
@@ -1,1 +1,12 @@
 #include "aho_corasick.h"
+
+namespace ac = aho_corasick;
+
+void ac::build(const std::vector<std::string>& dict)
+{
+	
+	for(std::vector<std::string>::const_iterator i = dict.begin(); i != dict.end(); ++i)
+	{
+
+	}
+}
--- aho_corasick.h
+++ aho_corasick.h
@@ -2,11 +2,49 @@
 #define __AHO_CORASICK_H__
 
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 
 namespace aho_corasick
 {
+	template<
+		range alphaHandler = lowerCase>
+	class trie
+	{
+	public:
+		explicit trie();
+		trie(const std::vector<std::string>&);
+
+		~trie();
+
+		void add(const std::string&);
+		void add(const std::vector<std::string>&);
+
+		// return the state entered from [q] by edge labeled [a], calls fail if no edge is found
+		int go(int q, char a);
+		// gets the node labeled by the longest proper suffix [w] on path [L] so that w is a prefix of some pattern
+		void fail(int q);
+		// output set of all patterns recognized when entering state [q]
+		std::set<std::string>& out(int q);
+
+	private:
+		// node (state) count
+		int states;
+	};
+
+	class range {
+	public:
+		int lower() const=0;
+		int upper() const=0; };
+
+	class lowerCase : range
+	{
+		static char first = 'a', last = 'z';
+	public:
+		static inline int lower() const { return first; }
+		static inline int upper() const { return last; }
+	};
 };
 
 #endif //end __AHO_CORASICK_H__
